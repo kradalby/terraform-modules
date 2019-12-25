1062d04b095a0c17f33834e176e7dd2c3ec1afd8
Kristoffer Dalby <kradalby@kradalby.no>
2019-12-25 10:22:55 +0000

    Add module for opnsense wireguard client on all sites

---
+++ opnsense_wireguard_all_sites_client/client.tf
@@ -0,0 +1,37 @@
+variable public_key {
+  type = string
+}
+variable name {
+  type = string
+}
+variable tunnel_address {
+  type = list(string)
+}
+
+resource "opnsense_wireguard_client" "opnsense_wireguard_client_terra" {
+  provider       = opnsense.terra
+  enabled        = true
+  name           = var.name
+  tunnel_address = var.tunnel_address
+  public_key     = var.public_key
+  shared_key     = ""
+  keep_alive     = 21
+}
+resource "opnsense_wireguard_client" "opnsense_wireguard_client_tjoda" {
+  provider       = opnsense.tjoda
+  enabled        = true
+  name           = var.name
+  tunnel_address = var.tunnel_address
+  public_key     = var.public_key
+  shared_key     = ""
+  keep_alive     = 21
+}
+resource "opnsense_wireguard_client" "opnsense_wireguard_client_ntnu" {
+  provider       = opnsense.ntnu
+  enabled        = true
+  name           = var.name
+  tunnel_address = var.tunnel_address
+  public_key     = var.public_key
+  shared_key     = ""
+  keep_alive     = 21
+}
