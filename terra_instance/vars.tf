93880a4146040d53d3d964d9143e902ea22ad4c7
Kristoffer Dalby <kradalby@kradalby.no>
2020-04-12 08:45:56 +0100

    Use fap domain id

--- terra_instance/vars.tf
+++ terra_instance/vars.tf
@@ -1,3 +1,8 @@
+variable "fap_no" {
+  type    = string
+  default = "6467f74f10e206068c311e586104ebe0"
+}
+
 variable "name" {
 }
 
--- vars.tf
+++ vars.tf
@@ -1,8 +1,3 @@
-variable "fap_no" {
-  type    = string
-  default = "6467f74f10e206068c311e586104ebe0"
-}
-
 variable "terra_datacenter" {
   type    = string
   default = "terra"
